ef08ca6637baea1b1ac8d1a0a4abfd5210f42f15
Nik495686 <nionnio1990@gmail.com>
2024-06-27 13:35:16 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use srict';
+// var Persone= {
+//     name:"Jone",
+//     age:25,
+//     isMarried:false
+  
+//   };
+// console.log(Persone["name"]);
+// const promt=require("prompt-sync")({sigint:true}) ;
+let Arr=['plum.png','orange.jpg'];
+console.log(Arr[0]);
+
+
+let answer = +prompt("Есть ли вам 18?","Да");
+
+console.log(typeof(answer));
+
+console.log("Arr"+"-object");
+console.log(4 +"-object");
+
+let incr =10,
+    decr=10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" ==2)
+
+let isChecked =false;
+    isClose=false;
+
+console.log(isChecked || isClose);
